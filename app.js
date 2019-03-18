8cb824bd34dbb2cd0d73e2c8fc93476cd519191a
Nawal00 <nawalgurung80@gmail.com>
2019-03-18 22:23:16 +0000

    contact section completed

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 $(() => {
 
+  //burger nav bar toggle active
   const $mainNav = $('#js-menu')
   const $navBarToggle = $('#js-navbar-toggle')
 
-  function toggleActive(){
-    $mainNav.toggleClass('active')
+  function toggleActive() {
+    $mainNav.toggleClass('burgerActive')
   }
 
   $navBarToggle.on('click', toggleActive)
 
+  // nav bar fixed on scroll
   $(window).scroll(function(){
     const wScroll = $(this).scrollTop()
 
@@ -27,5 +29,33 @@ $(() => {
       $('.navbar').removeClass('black')
     }
   })
+
+  // active nav-bar state on scroll
+  const
+    sections = $('section'),
+    nav = $('nav'),
+    navHeight = nav.outerHeight()
+
+  $(window).scroll(function () {
+    const curPos = $(this).scrollTop()
+
+    sections.each(function() {
+      const top = $(this).offset().top - navHeight
+      const bottom = top + $(this).outerHeight()
+
+      if (curPos >= top && curPos <= bottom) {
+        nav.find('a').removeClass('active')
+        sections.removeClass('active')
+
+        $(this).addClass('active')
+        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active')
+      } else if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        nav.find('a[href="#portfolio"]').removeClass('active')
+        nav.find('a[href="#contact-me"]').addClass('active')
+      }
+    })
+
+  })
+
 
 })
